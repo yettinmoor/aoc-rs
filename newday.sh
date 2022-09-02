50fd87bc17507e1d0a0b3b3051e22e494fd262f6
Nicolas Lindström <nicolas.lindstroem@protonmail.com>
2022-09-02 17:44:01 +0200

    newday

--- newday.sh
+++ newday.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env sh
 
+! [ "$#" -eq 2 ] && {
+    echo './newday <year> <day>'
+    exit 1
+}
+
 year=$1
 day=$2
 
 while true; do
-    read -r "add year$year::day$day? [y/N]" yn
+    echo "add year$year::day$day? [y/N] "
+    read -r yn
     case "$yn" in
         [yY]) break ;;
         *)    exit ;;
@@ -13,9 +19,14 @@ done
 
 year="year$year"
 day=$(printf 'day%02d' "$day")
+src_file="src/$year/$day.rs"
 
-touch "input/$year/$day.txt"
+[ -f "$src_file" ] && {
+    echo "src file $($src_file) exists!"
+    exit 1
+}
 
 cp src/common/template.rs "src/$year/$day.rs"
+echo "pub mod $day;" >> "src/$year.rs"
 
-echo "pub mod $day" >> "src/$year.rs"
+$EDITOR "${AOC_INPUT_DIR:-input}/$year/$day.txt"
